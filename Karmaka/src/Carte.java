171dfaa5b81dfc3f9bd496b5236270bf8406d3ff
BLSquared <lethug451@gmail.com>
2023-09-28 16:38:17 +0200

    Create Carte.java

---
+++ Karmaka/src/Carte.java
@@ -0,0 +1,21 @@
+package Karmaka.src;
+
+public class Carte {
+    
+    private int cout;
+
+    public int getCout(){
+        return cout;
+    }
+
+    public void setCout(int value){
+        cout = value;
+    }
+
+    public static void main(String[] args) {
+        Carte carte = new Carte();
+        carte.setCout(3);
+        System.out.println(carte.getCout());
+
+    }
+}
