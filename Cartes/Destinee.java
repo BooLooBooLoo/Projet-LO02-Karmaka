ab9b657e858b18d0b9086601c5944db2de6dcae5
Veitzen <hoang_viet.le@utt.fr>
2023-10-30 00:58:14 +0100

    add "Destinée" effect

--- Cartes/Destinee.java
+++ Cartes/Destinee.java
@@ -1,7 +1,11 @@
 package Cartes;
 
+import java.util.Scanner;
+
 import Karmaka.src.Carte;
 import Karmaka.src.Couleur;
+import Karmaka.src.Partie;
+import Karmaka.src.Pile;
 
 public class Destinee extends Carte{
 	
@@ -10,8 +14,41 @@ public class Destinee extends Carte{
 	}
 
 	@Override
-	public void effet() {
-		// TODO Auto-generated method stub
-		System.out.println("J'aime la pêche");
-	}
+	public void effet(Partie partie) {
+		// Déclaration des variables utilisés dans cette classe
+		Pile Main = partie.getTour().getMain();
+		Pile Source = partie.getSource();
+		Pile VieFuture = partie.getTour().getVieFuture();
+		Scanner sc = new Scanner(System.in);
+		// Syso et Scan 
+		System.out.println("3 Premières Cartes de la Source :");
+		for(int i=0; i<3; i++) {
+			if(i>Source.getCartes().size()) {
+				break;
+			}
+			System.out.println(Source.getCartes().get(i).getNom());
+		}
+		System.out.println("Combien de carte à ajouter dans la Vie Future?");
+		int nbCarte = sc.nextInt();
+		for(int i=0; i<nbCarte; i++) {
+			System.out.println("Choisir une carte à placer dans votre main.");
+			String carteSelect = sc.nextLine();
+			// Trouver la carte sélectionnée
+			int indiceCarteSelect = -1;
+			for(int j=0; j<Source.getCartes().size(); j++) {
+				if(Source.getCartes().get(j).getNom().equals(carteSelect)) {
+					indiceCarteSelect = j;
+					break;
+				}
+			}
+			// Modification objet "partie"
+			if(indiceCarteSelect == -1) {
+				System.out.println("Erreur! (La carte n'est pas trouvé...)");
+			} else {
+				VieFuture.addCarte(Source.getCartes().get(indiceCarteSelect));
+			}		
+		}
+		sc.close();
+		Main.removeCarte(this);
+		}
 }
