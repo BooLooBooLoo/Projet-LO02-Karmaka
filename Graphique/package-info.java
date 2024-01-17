937b1bfa0c94fa5bddb759d295b66bfb6cc4b652
Veitzen <hoang_viet.le@utt.fr>
2024-01-18 00:17:55 +0100

    fix and complete javadoc

--- Graphique/package-info.java
+++ Graphique/package-info.java
@@ -1,6 +1,6 @@
 /**
  *  Le package {@code Graphique} contient l'ensemble des classes qui permet à l'interface graphique de fonctionner correctement.
- *  Dans ce package, il y a le Controller et la Vue qui permettent de réalise le patron MVC utilisé dans notre projet pour l'interface.
+ *  Dans ce package, il y a le Controller et la Vue qui permettent de réaliser le patron MVC utilisé dans notre projet pour l'interface.
  *  Il y a une autre classe qui permet d'afficher des images de fond sur un JPanel qui est ImagePanel.
  *  
  * @author Ali MIKOU et Hoang-Viet LE
